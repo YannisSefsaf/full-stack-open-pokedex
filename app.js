bbbb7b0187634988a2a63efe57c954b59b9c1abd
YannisSefsaf <111019910+YannisSefsaf@users.noreply.github.com>
2023-12-30 18:57:36 -0600

    11.9

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000;
 
+// Serve static files from the 'dist' directory
 app.use(express.static("dist"));
 
+// Add a catch-all route that sends back the index.html file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 app.listen(PORT, () => {
-  console.log("server started on port 5000");
+  console.log(`Server started on port ${PORT}`);
 });
