bc3ff182a750f9d09bb62b8079ec6fd3f4356b20
YannisSefsaf <111019910+YannisSefsaf@users.noreply.github.com>
2024-01-02 20:37:23 -0600

    Simulate error and rollback #8

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Serve static files from the 'dist' directory
 app.use(express.static("dist"));
