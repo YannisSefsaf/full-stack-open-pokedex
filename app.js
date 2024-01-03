4d096b5e231e9cabfa771538d02d58831182d008
YannisSefsaf <111019910+YannisSefsaf@users.noreply.github.com>
2024-01-02 19:16:39 -0600

    Simulate error and rollback

--- app.js
+++ app.js
@@ -8,7 +8,19 @@ const PORT = process.env.PORT || 5000;
 // Serve static files from the 'dist' directory
 app.use(express.static("dist"));
 
-// Add a catch-all route that sends back the index.html file
+// Health check endpoint
+app.get("/health", (req, res) => {
+  throw "error...";
+  // eslint-disable-next-line no-unreachable
+  res.send("ok");
+});
+
+// Version endpoint
+app.get("/version", (req, res) => {
+  res.send("1"); // Increment this number with each new deployment
+});
+
+// Catch-all route for serving the main index.html
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
