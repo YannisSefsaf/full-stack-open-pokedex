8c9870a69f5179b24d7e031a8bf51349d377e415
YannisSefsaf <111019910+YannisSefsaf@users.noreply.github.com>
2024-01-02 23:14:36 -0600

    #14

--- app.js
+++ app.js
@@ -9,10 +9,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static("dist"));
 
 // Health check endpoint
-app.get("/health", (/* req, res */) => {
-  throw new Error("error!");
-  /*   // eslint-disable-next-line no-unreachable
-  res.send("ok"); */
+app.get("/health", (req, res) => {
+  res.status(500).send("error");
 });
 
 // Version endpoint
